582300cd55453d5682c0b8317ba07733e0c4c0ba
Ymanbek Baltabaev Eralievich <ymanbekbaltabaev17@gmail.com>
2026-04-12 06:25:10 +0600

    Resolved conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { App } from './App.jsx'
 import Themes from './components/Themes.jsx'
-import {App} from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <Themes>
-    <App/>
+    <App />
   </Themes>
 )
