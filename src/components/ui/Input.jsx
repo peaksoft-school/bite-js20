52d3ed96e0b8939d700b714511411e776a43af93
Kursant Anarkulov <anarkursant08@gmail.com>
2026-04-14 20:34:08 +0600

    feat(ui): add Input component styles

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,33 +1,87 @@
-import { Box, InputBase, Typography } from '@mui/material'
+import { InputBase, styled } from '@mui/material'
 
 export const Input = ({
+  id,
   type = 'text',
   placeholder,
   label,
   error,
   helperText,
   disabled,
-  multiline,
-  rows,
   value,
   onChange,
   ...props
 }) => (
-  <Box>
-    {label && <label>{label}</label>}
+  <div id="12">
+    {label && <label htmlFor={id}>{label}</label>}
 
-    <InputBase
+    <StyledInput
       type={type}
       placeholder={placeholder}
       error={error}
       disabled={disabled}
-      multiline={multiline}
-      rows={rows}
       value={value}
       onChange={onChange}
       {...props}
     />
 
-    {helperText && <Typography component="span">{helperText}</Typography>}
-  </Box>
+    {helperText && <HelperText component="span">{helperText}</HelperText>}
+  </div>
 )
+
+const StyledInput = styled(InputBase)(({ theme }) => {
+  const { primary, secondary, error } = theme.palette
+
+  return {
+    width: '100%',
+    height: '2.875rem',
+    borderRadius: '0.5rem',
+    padding: '0.5rem 1.25rem',
+
+    border: `1px solid ${secondary.greyMid}`,
+    backgroundColor: primary.white,
+
+    fontFamily: 'Helvetica',
+    fontWeight: '400',
+    lineHeight: '100%',
+    fontSize: '1rem',
+
+    '& .MuiInputBase-input': {
+      padding: 0,
+      color: primary.black,
+
+      '&::placeholder': {
+        color: secondary.greyMid,
+        opacity: 1,
+      },
+    },
+
+    '&:hover': {
+      border: `1px solid ${primary.black}`,
+    },
+
+    '&.Mui-focused': {
+      border: `1px solid ${primary.black}`,
+    },
+
+    '&.Mui-error': {
+      border: `1px solid ${error.main}`,
+
+      '&:hover': {
+        border: `1px solid ${error.main}`,
+      },
+    },
+
+    '&.Mui-disabled': {
+      backgroundColor: primary.greyDark,
+      border: `1px solid ${secondary.greyMid}`,
+    },
+  }
+})
+
+const HelperText = styled('span')(({ theme, error }) => ({
+  fontSize: '0.75rem',
+  marginTop: '0.25rem',
+  display: 'block',
+  color: error ? theme.palette.error.main : theme.palette.secondary.greyMid,
+}))
