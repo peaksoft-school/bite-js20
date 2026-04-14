ab3740c39506dad60a60d4965ffb5f928d948c7b
Kursant Anarkulov <anarkursant08@gmail.com>
2026-04-14 14:29:01 +0600

    feat(ui): set up Input component props

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import { Box, InputBase, Typography } from '@mui/material'
 
-export const Input = () => {
-  return <input />
-}
+export const Input = ({
+  type = 'text',
+  placeholder,
+  label,
+  error,
+  helperText,
+  disabled,
+  multiline,
+  rows,
+  value,
+  onChange,
+  ...props
+}) => (
+  <Box>
+    {label && <label>{label}</label>}
+
+    <InputBase
+      type={type}
+      placeholder={placeholder}
+      error={error}
+      disabled={disabled}
+      multiline={multiline}
+      rows={rows}
+      value={value}
+      onChange={onChange}
+      {...props}
+    />
+
+    {helperText && <Typography component="span">{helperText}</Typography>}
+  </Box>
+)
