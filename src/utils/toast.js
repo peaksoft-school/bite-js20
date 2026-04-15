6ef66865cf75b321fdc81c2a1c7212fc8452c5a9
Ruslan-007 <erkebaevruslan385@gmail.com>
2026-04-15 13:56:11 +0600

    delete comments

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,21 +1,17 @@
 import { toast } from 'react-toastify'
 
-//  SUCCESS
 export const showSuccess = ({ title, message }) => {
   toast.success(`${title} — ${message}`)
 }
 
-//  ERROR
 export const showError = ({ title, message }) => {
   toast.error(`${title} — ${message}`)
 }
 
-//  INFO
 export const showInfo = ({ title, message }) => {
   toast.info(`${title} — ${message}`)
 }
 
-//  WARNING
 export const showWarning = ({ title, message }) => {
   toast.warning(`${title} — ${message}`)
 }
