4e2af07291c90d7a9122c737718138074b789870
Kursant Anarkulov <anarkursant08@gmail.com>
2026-04-19 13:06:41 +0600

    style(auth): move background image to StyledBox and set background-size to contain

--- src/components/auth/AuthPage.jsx
+++ src/components/auth/AuthPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { Box, styled } from '@mui/material'
+import { LoginForm } from './LoginForm'
+import { RegisterForm } from './RegisterForm'
+import background from '../../assets/images/bite-background.png'
 
 export const AuthPage = () => {
   return (
-    <div>AuthPage</div>
+    <StyledBox>
+        <LoginForm />
+        <RegisterForm />
+    </StyledBox>
   )
 }
+
+const StyledBox = styled(Box)({
+  width: "100vw",
+  height: "100vh",
+
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  backgroundSize: 'contain',
+
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
