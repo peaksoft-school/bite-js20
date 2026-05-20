429d7ae7e6b0fd3526b66c11bb43ea87b67c3d67
Kursant Anarkulov <anarkursant08@gmail.com>
2026-05-20 18:50:10 +0600

    feat: wrap App component with GoogleOAuthProvider for authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { App } from './App.jsx'
 import Themes from './components/Themes.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <Themes>
-    <App />
-  </Themes>
+  <GoogleOAuthProvider clientId="212866824382-v98iof4a1p1argfi99v2rbq4o30cs7gg.apps.googleusercontent.com">
+    <Themes>
+      <App />
+    </Themes>
+  </GoogleOAuthProvider>
 )
