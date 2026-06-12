dcabe35c5503c71d0bb20a06f16d7333ad5612ee
Kursant Anarkulov <anarkursant08@gmail.com>
2026-06-12 13:12:15 +0600

    feat(main): wrap App component with Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx'
 import Themes from './components/Themes.jsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <Themes>
-    <App />
-  </Themes>
+  <Provider store={store}>
+    <Themes>
+      <App />
+    </Themes>
+  </Provider>
 )
